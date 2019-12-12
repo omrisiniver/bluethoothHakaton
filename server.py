cdc0ff8c6e5b2b2f31c77dffa532f17ad9a77d26
Yuval Goldberg <yuvigoldi@hotmail.com>
2019-12-12 11:33:51 +0200

    changes in server

--- server.py
+++ server.py
@@ -9,6 +9,7 @@ import pickle
 from threading import Thread, RLock
 import json
 import requests
+import collections
 url = 'https://theroom-1df52.firebaseio.com/alarm.json'
 
 SERVER_IP = '192.168.4.39'
@@ -21,9 +22,14 @@ sock.bind((SERVER_IP, SERVER_PORT))
 data = {}
 enemy = False
 lock = RLock()
-close_location = 'close_from_door'
-far_location = 'far_from_door'
+IN_DOOR = 'in door'
+OUT_DOOR = 'out door'
 is_started = False
+
+class Packet:
+    def __init__(self, rssi, timestamp):
+        self.rssi = rssi
+        self.timestamp = timestamp
 
 class DataCollector(Thread):
     def __init__(self, data, is_started, lock):
@@ -48,52 +54,80 @@ class DataCollector(Thread):
                 packet = sock.recv(100)
                 info = pickle.loads(packet)
                 mac = info["location"]
+                packet = Packet(rssi=info['rssi'], timestamp=info['timestamp'])
                 if mac in self.data:
                     if self.is_started:
-                        if mac == 'close_from_door':
+                        if mac == IN_DOOR:
                             self.close_packet_counter += 1
                         else:
                             self.far_packet_counter += 1
-                        self.data[mac].append(info['rssi'])
+                        
                 else:
-                    print('arrived {} {}'.format(mac, count_sensors))
+                    # print('arrived {} {}'.format(mac, count_sensors))
                     count_sensors += 1
                     if count_sensors == 2:
-                        print('arrived True')
                         self.is_started = True
-                    self.data[mac] = [info['rssi']]
+                    self.data[mac] = collections.OrderedDict()
+                self.data[mac][packet.timestamp] = packet.rssi
+
+def calc_grad(data):
+    batches = data / 10
+
+class Server(Thread):
+
+    def run(self):
+        global data
+
+        counter = 0
+        while True:
+            if data_collector.get_started() is False:
+                continue
+
+            curr_far, curr_close = data_collector.get_packet_counter()
+            if curr_close < counter or curr_far < counter:
+                # print('stuck {} {}'.format(curr_close, curr_far))
+                continue
+            prev_check = enemy
+
+            in_door = data[IN_DOOR]
+            out_door = data[OUT_DOOR]
+
+            delta = in_door - out_door
+
+            # Graph change over time
+            in_gradient = calc_grad(in_door)
+            out_gradient = calc_grad(out_door)
+
+            if in_door > out_door:
+                enemy = False
+                print("Enemy is outside")
+            else:
+                enemy = True
+                print("Enemy is inside")
+
+            # print('far: {}, close: {}'.format(data['far_from_door'][counter], data['close_from_door'][counter]))
+            # if data['far_from_door'][counter] + THRESHOLD > 0:
+            #     enemy = True
+            # elif data['close_from_door'][counter] + THRESHOLD < 0 and \
+            #      data['far_from_door'][counter] + THRESHOLD * 2 < 0:
+            #     enemy = False
+            # elif data['close_from_door'][counter] + THRESHOLD > 0 and \
+            #      data['far_from_door'][counter] + THRESHOLD * 2 > 0:
+            #     enemy = True
+            # # elif data['close_from_door'][counter] + THRESHOLD > 0 and \
+            #      data['far_from_door'][counter] + THRESHOLD * 2 > 0:
+            #     enemy = True
+            counter += 1
+            if enemy is not prev_check:
+                status = {'state': enemy}
+                r = requests.put(url, data=json.dumps(status))
+                print("Enemy is : {}".format(enemy))
 
 
 if __name__ == "__main__":
     # global data
     data_collector = DataCollector(data=data, is_started=is_started, lock=lock)
     data_collector.start()
-    counter = 0
-
-    while True:
-        if data_collector.get_started() is False:
-            continue
-
-        curr_far, curr_close = data_collector.get_packet_counter()
-        if curr_close < counter or curr_far < counter:
-            # print('stuck {} {}'.format(curr_close, curr_far))
-            continue
-        prev_check = enemy
-
-        print('far: {}, close: {}'.format(data['far_from_door'][counter], data['close_from_door'][counter]))
-        if data['far_from_door'][counter] + THRESHOLD > 0:
-            enemy = True
-        elif data['close_from_door'][counter] + THRESHOLD < 0 and \
-             data['far_from_door'][counter] + THRESHOLD * 2 < 0:
-            enemy = False
-        elif data['close_from_door'][counter] + THRESHOLD > 0 and \
-             data['far_from_door'][counter] + THRESHOLD * 2 > 0:
-            enemy = True
-        # elif data['close_from_door'][counter] + THRESHOLD > 0 and \
-        #      data['far_from_door'][counter] + THRESHOLD * 2 > 0:
-        #     enemy = True
-        counter += 1
-        if enemy is not prev_check:
-            status = {'state': enemy}
-            r = requests.put(url, data=json.dumps(status))
-            print("Enemy is : {}".format(enemy))
+    
+    server = Server()
+    server.start()
